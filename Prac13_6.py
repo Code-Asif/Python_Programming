c09713bf555fe2f699df8bfefcd15d0c1f8d0d2b
Asif Alam <safety.asif.alam@gmail.com>
2023-10-10 01:24:58 +0530

    modified:   Prac13_6.py

--- Prac13_6.py
+++ Prac13_6.py
@@ -1,16 +1,15 @@
 #Python Program to Check if a Given String is Palindrome.
 
-def isPalindrome(str):
-    if len(str) == 0 or len(str) == 1:
-        return True
+def isPalindrome(str, n):
+    j= n-1
+    str2 = ""
+    for i in range(n):
+        str2 += str[j]
+        j -= 1
+    if str2 == str:
+        print("String is palindrome")
     else:
-        if str[0] == str[-1]:
-            return isPalindrome(str[1:-1])
-        else:
-            return False
+        print("String is not palindrome")
 
 str = input("Enter a string: ")
-if isPalindrome(str):
-    print("String is palindrome")
-else:
-    print("String is not palindrome")
+isPalindrome(str, len(str))
