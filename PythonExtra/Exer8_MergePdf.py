a8ef4c1a37743ed794bd79965eaa6b717457445b
Asif Alam <safety.asif.alam@gmail.com>
2023-11-11 03:33:35 +0530

    new file:   1.pdf 	new file:   2.pdf 	new file:   PythonExtra/Exer8_MergePdf.py 	new file:   merged2.pdf

---
+++ PythonExtra/Exer8_MergePdf.py
@@ -0,0 +1,12 @@
+# Merging pdf`s using pypdf2
+from PyPDF2 import PdfWriter
+import os
+
+merger = PdfWriter()
+files = [file for file in os.listdir() if file.endswith(".pdf")]
+
+for pdf in files:
+    merger.append(pdf)
+
+merger.write("merged2.pdf")
+merger.close()
