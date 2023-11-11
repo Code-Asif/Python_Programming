c22c60cffe182daeb6a20df6f31bc1c03acd0d6d
Asif Alam <safety.asif.alam@gmail.com>
2023-11-11 23:06:06 +0530

    new file:   PythonExtra/shutilDemo.py 	renamed:    book_data/1.txt -> book_data/101.txt 	modified:   book_data/2.txt

---
+++ PythonExtra/shutilDemo.py
@@ -0,0 +1,11 @@
+# use of shutil library
+
+import shutil
+
+# shutil.copy("101.txt","102.txt")
+# shutil.copy2("101.txt","102.txt")
+# shutil.rmtree("101.txt","102.txt")
+# shutil.move("101.txt","102.txt")
+# shutil.copytree("101.txt","102.txt")
+
+shutil.copy("./book_data/101.txt","./book_data/2.txt")
