225ff1449f87b73e09eeaa8b89cf83f6d4f19a06
Asif Alam <safety.asif.alam@gmail.com>
2023-11-13 12:57:42 +0530

    new file:   PythonExtra/ConcurrentDownload.py 	modified:   PythonExtra/News.py

--- PythonExtra/News.py
+++ PythonExtra/News.py
@@ -7,7 +7,7 @@ url = f"https://newsapi.org/v2/everything?q={query}&from=2023-10-12&sortBy=publi
 
 r = requests.get(url)
 news = json.loads(r.text)
-# print(news, type(news))
+
 for article in news["articles"]:
     print(f"{i}.Title: ", end=" ")
     print(article["title"])
